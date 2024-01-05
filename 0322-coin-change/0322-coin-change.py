84c93eb0ffca2b45d395f82a1e607a562cc6c606
parkjunhada <63660112+junha6316@users.noreply.github.com>
2024-01-05 09:57:22 +0900

    Time: 770 ms (92.02%), Space: 17.7 MB (37.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,19 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        """
-        가장 적은 조합으롣 amount를 만족 시켜야됨
-        """
+        dp = [float('inf') for _ in range(amount+1)]
+        dp[0] = 0 
         
-        dp = [float('inf')] * (amount +1)
-        
-        dp[0] = 0
         for coin in coins:
-            for x in range(coin, amount+1):
-                dp[x] = min(dp[x], dp[x-coin] +1)
-        return dp[amount] if dp[amount] != float('inf') else -1
-        
-        
-        
-        
-        
+            for x in range(coin, amount +1):
+                dp[x] = min(dp[x], dp[x-coin] +1 )
+                
+        return -1 if dp[-1] == float('inf') else dp[-1]
+            
         
