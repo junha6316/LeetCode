324f6a5c952ba3b0d7a17f62520e63ee81ce4cf4
parkjunhada <63660112+junha6316@users.noreply.github.com>
2024-01-05 09:03:50 +0900

    Time: 30 ms (93.08%), Space: 17.2 MB (15.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0 for _ in range(n+1)] # dp[i] i번째에 도달 할 수 있는 방법
+        dp = [0 for i in range(n+1)]
+        
         dp[0] = 1
         dp[1] = 1
         
@@ -8,11 +9,6 @@ class Solution:
         for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
             
-        return dp[n]
-            
-        
-    
-
-                
+        return dp[-1]
 
         
