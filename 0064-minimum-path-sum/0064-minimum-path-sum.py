594259bbfd0ed7b9b17f437083f8ab3a0885f7c6
parkjunhada <63660112+junha6316@users.noreply.github.com>
2024-01-06 10:26:55 +0900

    Time: 77 ms (98.07%), Space: 19.3 MB (16.97%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,20 +4,24 @@ class Solution:
             return 0
 
         m, n = len(grid), len(grid[0])
-        dp = [[0] * n for _ in range(m)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
         dp[0][0] = grid[0][0]
-
-        # Fill the first row and first column
-        for i in range(1, m):
-            dp[i][0] = dp[i - 1][0] + grid[i][0]
-        for j in range(1, n):
-            dp[0][j] = dp[0][j - 1] + grid[0][j]
-
-        # Fill the rest of the DP table
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-
-        return dp[m - 1][n - 1]
+        for c in range(1, n):
+            dp[0][c] += dp[0][c-1] + grid[0][c]
+            
+        for r in range(1, m):
+            dp[r][0] += dp[r-1][0] + grid[r][0]
+            
+        
+        
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = min(dp[r-1][c], dp[r][c-1]) + grid[r][c]
+                
+        return dp[-1][-1]
+            
+       
                 
         
