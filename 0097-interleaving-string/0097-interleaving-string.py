986359ace3134e355b853cf0dd9d8d5e85b09b37
parkjunhada <63660112+junha6316@users.noreply.github.com>
2024-01-07 11:34:44 +0900

    Time: 52 ms (40.59%), Space: 17.6 MB (22.72%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -6,20 +6,19 @@ class Solution:
         if n1 + n2 != len(s3):
             return False
         
-        dp = [[False for _ in range(n2+1)] for _ in range(n1+1)]
-        
-        dp[0][0] =True
-        
         if n1 == 0 or n2 ==0:
             if n1 == 0:
                 return s3== s2
             if n2 ==0:
                 return s3 ==s1
-            
+        
+        dp = [[False for _ in range(n2+1)] for _ in range(n1+1)]
+        dp[0][0] =True
+        
         
         for i in range(n1+1):
             for j in range(n2+1):
-                if dp[i-1][j] and s1[i-1] == s3[i+j-1]:
+                if dp[i-1][j] and s1[i-1] == s3[i+j-1]: # s1, s3입장에서는 -1를 해줘야 맞는 인덱스가 들어감
                     dp[i][j] = True
                 
                 if dp[i][j-1] and s2[j-1] == s3[i+j-1]:
