c22ee0980d663d8dca081557b59c66089aeb551e
parkjunhada <63660112+junha6316@users.noreply.github.com>
2024-01-12 08:54:33 +0900

    Time: 67 ms (59.94%), Space: 18.8 MB (18.76%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,16 +1,23 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [[0 for i in r] for r in triangle]
+        """
+        dp[i][j] = min(dp[i-1][j], dp[i-1][j]) + nums[i][j]
+        """
+        dp = [[0 for i in range(len(row))] for row in triangle]
+        # initialize
         dp[0][0] = triangle[0][0]
-        
-        for l in range(1, len(triangle)):
-            row = triangle[l]
-            for idx, num in enumerate(row):
-                if idx == 0:
-                    dp[l][idx] = dp[l-1][0] + num
-                elif idx == len(row) -1:
-                    dp[l][idx] = dp[l-1][-1] + num
-                else:
-                    dp[l][idx] = min(dp[l-1][idx], dp[l-1][idx-1]) + num
-                    
+        for i in range(1, len(triangle)):
+            dp[i][0] = triangle[i][0] + dp[i-1][0]
+            dp[i][-1] = triangle[i][-1] + dp[i-1][-1]
+            
+        for i in range(1, len(triangle)):
+            row = triangle[i]
+            for j in range(1, len(row)-1):
+                dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+
+            
+            
         return min(dp[-1])
+        
+            
+        
